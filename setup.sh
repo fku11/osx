851790cb27d6619e5b0ca78636aec6a4e4b5ed24
fku11 <fku11@devnullmail.com>
2015-10-04 01:14:56 +0500

    not deleting git info

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/fku11/osx ~/.fku
-rm -rf ~/.fku/.git ~/.fku/setup.sh
 sed -i -e "s/username/`whoami`/g" ~/.fku/com.fku.python.plist
 cp ~/.fku/com.fku.python.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/com.fku.python.plist

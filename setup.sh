a148e8b9af63e34284d07b1978b244e58041c55d
fku11 <fku11@devnullmail.com>
2016-09-07 01:51:39 +0500

    Better Clear

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ printf '\e[8;1;1t'
 printf '\e[3;100000;100000t'
 printf '\e[2t'
 
-set -e           # exit on error
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o errexit   # exit the script if any statement returns a non-true return value
+set -e
+set -o pipefail
+set -o errtrace
+set -o errexit
 
 unset HISTFILE
 
@@ -24,6 +24,6 @@ cp ~/.fku/com.fku.python.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/com.fku.python.plist
 launchctl start com.fku.python
 
-clear
+clear && printf '\e[3J'
 ps aux | grep -i "Terminal" | xargs kill -9
 
